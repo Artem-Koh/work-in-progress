71d15366c721d7bfee632a650ca6e215ba9ded68
Kenny-Mc-cormick <73953347+Kenny-Mc-cormick@users.noreply.github.com>
2020-11-13 12:20:47 +0300

    Update сама работа.py

--- сама работа.py
+++ сама работа.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib3
+import datetime
 
 h = urllib3.PoolManager()
 resp = h.request('GET', 'https://coronavirus-monitor.ru/coronavirus-v-moskve/')
 a = resp.data.decode('utf-8')
 soup = BeautifulSoup(a, 'lxml')
 c = str(soup.select("strong:nth-of-type(3)")).strip('[<strong>увеличилось на ')
-print(c.strip('</strong>]'))
+c = c.strip('</strong>]')
+print(c)
+
+with open (r'C:\Users\bbrit\Desktop\data.csv', 'a') as f:
+    f.write(c + ', ' + datetime.datetime.today().strftime("%Y-%m-%d") + '\n')

72da9788ac4df7e62c13660d7f98079f05e696d0
Kenny-Mc-cormick <73953347+Kenny-Mc-cormick@users.noreply.github.com>
2020-11-18 08:24:54 +0300

    Update сама работа.py

--- сама работа.py
+++ сама работа.py
@@ -9,9 +9,11 @@ a = resp.data.decode('utf-8')
 soup = BeautifulSoup(a, 'lxml')
 c = str(soup.select("strong:nth-of-type(3)")).strip('[<strong>увеличилось на ')
 c = c.strip('</strong>]')
+x = os.path.join('C:\\','Users','bbrit','Desktop','data.csv')
 if c == 'зменилось':
     print('Сайт не обновил данные')
     
 else:
-    with open (r'C:\Users\bbrit\Desktop\data.csv', 'a') as f:
-        f.write(c + ',' + datetime.datetime.today().strftime("%Y-%m-%d") + '\n')
+    with open (x, 'a') as f:
+        f.write(c + ', ' + datetime.datetime.today().strftime("%Y-%m-%d") + '\n')
+        print('Дело сделано')

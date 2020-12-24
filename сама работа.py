53d155960292011660036ba7da070a44a987258e
Kenny-Mc-cormick <73953347+Kenny-Mc-cormick@users.noreply.github.com>
2020-12-24 17:13:49 +0300

    Update сама работа.py

--- сама работа.py
+++ сама работа.py
@@ -3,26 +3,23 @@ import urllib3
 import os
 import datetime
 
-h = urllib3.PoolManager()
-resp = h.request('GET', 'https://coronavirus-control.ru/coronavirus-moscow/')
+connection = urllib3.PoolManager()
+resp = connection.request('GET', 'https://coronavirus-control.ru/coronavirus-moscow/')
 a = resp.data.decode('utf-8')
 soup = BeautifulSoup(a, 'lxml')
-c = str(soup.article.b.span).strip('<span><span class="plus">+').strip('</span></span>')
-print(c)
-k = str(soup.article.b).strip('<b>')
-k = k[:12].replace(' ', '')
-print(k)
-o = str(soup.article.div.select('div:nth-of-type(2)'))
-for i in range(len(o) - 300):
-        if o[i:i + 10] == 'На сегодня':
-                d = o[i + 14:i + 24]
+quantity = str(soup.article.b.span).strip('<span><span class="plus">+').strip('</span></span>')
+new_cases = str(soup.article.b).strip('<b>')
+new_cases = new_cases[:12].replace(' ', '')
+date = str(soup.article.div.select('div:nth-of-type(2)'))
+for i in range(len(date) - 300):
+        if date[i:i + 10] == 'На сегодня':
+                date = date[i + 14:i + 24]
                 break
-print(d)
-t = datetime.datetime.today().strftime("%d.%m.%Y - %H:%M")
-print(t)
-x = os.path.join('C:\\','Users','bbrit','Desktop','data_corona.csv')
-with open (x, 'a') as f:
-        f.write(c + ', ' + k + ', ' + o + ', ' + t + '\n')
+time = datetime.datetime.today().strftime("%d.%m.%Y - %H:%M")
+
+the_way = os.path.join('C:\\','Users','bbrit','Desktop','data_corona.csv')
+with open (the_way, 'a') as file:
+        file.write(quantity + ', ' + new_cases + ', ' + date + ', ' + time + '\n')
         print('Дело сделано')
 
 

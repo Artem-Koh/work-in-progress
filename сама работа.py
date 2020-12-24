95d22a555dd5f85483662934134e96600905be8d
Kenny-Mc-cormick <73953347+Kenny-Mc-cormick@users.noreply.github.com>
2020-12-24 17:07:23 +0300

    Add files via upload

--- сама работа.py
+++ сама работа.py
@@ -4,16 +4,25 @@ import os
 import datetime
 
 h = urllib3.PoolManager()
-resp = h.request('GET', 'https://coronavirus-monitor.ru/coronavirus-v-moskve/')
+resp = h.request('GET', 'https://coronavirus-control.ru/coronavirus-moscow/')
 a = resp.data.decode('utf-8')
 soup = BeautifulSoup(a, 'lxml')
-c = str(soup.select("strong:nth-of-type(3)")).strip('[<strong>увеличилось на ')
-c = c.strip('</strong>]')
-x = os.path.join('C:\\','Users','bbrit','Desktop','data.csv')
-if c == 'зменилось':
-    print('Сайт не обновил данные')
-    
-else:
-    with open (x, 'a') as f:
-        f.write(c + ', ' + datetime.datetime.today().strftime("%Y-%m-%d") + '\n')
+c = str(soup.article.b.span).strip('<span><span class="plus">+').strip('</span></span>')
+print(c)
+k = str(soup.article.b).strip('<b>')
+k = k[:12].replace(' ', '')
+print(k)
+o = str(soup.article.div.select('div:nth-of-type(2)'))
+for i in range(len(o) - 300):
+        if o[i:i + 10] == 'На сегодня':
+                d = o[i + 14:i + 24]
+                break
+print(d)
+t = datetime.datetime.today().strftime("%d.%m.%Y - %H:%M")
+print(t)
+x = os.path.join('C:\\','Users','bbrit','Desktop','data_corona.csv')
+with open (x, 'a') as f:
+        f.write(c + ', ' + k + ', ' + o + ', ' + t + '\n')
         print('Дело сделано')
+
+
